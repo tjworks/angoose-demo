8d6e98d6c16f1b33c5106321824a3459f14c0724
TJ <evertang@gmail.com>
2014-02-27 23:19:20 -0500

    add angoose-ui demo

--- server/models/event.js
+++ server/models/event.js
@@ -4,14 +4,11 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var    Mixed = mongoose.Schema.Types.Mixed;
 
 var EventSchema = new mongoose.Schema({
-    event_data: {
         ts: Date,
         name: String,
         value:Mixed,
         desc: String,
         tags: [String],
-        actor: { _id: ObjectId }
-     },
     type: {type: String, default: 'event', editable: false}
 }, {
     collection: 'events'
